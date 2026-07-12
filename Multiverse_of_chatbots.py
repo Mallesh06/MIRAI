c005b59f5806bc56c539c465908a31d127bea07c
Mallesh06 <malleshpeddagolla06@gmail.com>
2026-07-12 15:52:06 +0530

    adding session state and chat message

--- Multiverse_of_chatbots.py
+++ Multiverse_of_chatbots.py
@@ -1,33 +1,56 @@
+#---------import libraries----------------
 import streamlit as st
 
+st.sidebar.title("THE MULTIVERSE OF CHATBOTS")
 st.title("THE MULTIVERSE OF CHATBOTS")
-personality=st.selectbox("Who do you want to talk to?. ",
-    ["Mr. cool dhoni",
+st.subheader("Hey,  Whats on your mind?...")
+personality=st.sidebar.selectbox("Who do you want to talk to?. ",
+    ["Normal",
+    "Mr. cool dhoni",
     "The torchberer Rajamouli",
     "The masterpeice Hans zimmer",
     "The goat Vijay Thalapathi",]
 )
+#-----------adding intensity---------
+intensity=st.sidebar.slider("Intensity",min_value=1, max_value=10, value=5)
 
+# ------import libraries------
 import os
 from google import genai
 from dotenv import load_dotenv
+
+#-------adding API key ---------
 load_dotenv()
 client=genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-user_message=st.text_input("Ask a question.")
-if st.button("Send"):
-    if user_message:
-        ai_instruction=f"You are acting as {personality}. respond to the message sent by the user staying complete in that character: {user_message}"
-        with st.spinner("sit tight!, we are working on it..."):
-            response=client.models.generate_content(
-                model="gemini-2.5-flash",
-                contents=ai_instruction
-            )
+#---------chat history------------
+if "chat_history" not in st.session_state :
+    st.session_state.chat_history = []
 
-            st.success("Message received!")
-            st.write(response.text)
-    else:
-        st.warning("Please type something!")
+
+#---------user message--------------------
+user_message =st.chat_input("Say Something........")
+if user_message :
+    ai_instruction=f"You are acting as {personality} with the intensity of {intensity}. respond to the message sent by the user staying complete in that character: {user_message}"
+    with st.spinner("sit tight!, we are working on it..."):
+        response=client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=ai_instruction
+            )
+        
+# ---------save conversation in session state------------
+        st.session_state.chat_history.append(("user",user_message))
+        st.session_state.chat_history.append(("ai",response.text))
+
+#-----------displaying history---------------------
+for role, msg in st.session_state.chat_history:
+    if role =="user":
+        st.chat_message("user").write(msg)
+    elif role=="ai":
+        st.chat_message("ai").write(msg)
+
+
+
 
 
 
